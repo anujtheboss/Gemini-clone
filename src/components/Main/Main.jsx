f67f5be77aad106cbeb8c16f77b1bafe8663f746
anujtheboss <anujth345@gmail.com>
2025-05-15 16:08:37 +0545

    highlight.js and marked packages used for highlight

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,72 @@
-import React, { useContext } from 'react'
+import React, { useContext,useEffect } from 'react'
+import hljs from 'highlight.js'
+import { marked } from 'marked'
+import { markedHighlight } from 'marked-highlight'
+import 'highlight.js/styles/github.css'  // Change this line
 import './Main.css'
 import { assets } from '../../assets/assets'
 import { Context } from '../../context/context'
 const Main = () => {
     const { onSent, recentPrompt, showResult, loading, resultData, setInput, input } = useContext(Context)
+    // useEffect(() => {
+    //     marked.setOptions({
+    //         highlight: function (code, lang) {
+    //             if (lang && hljs.getLanguage(lang)) {
+    //                 return hljs.highlight(code, { language: lang }).value
+    //             }
+    //             return hljs.highlightAuto(code).value
+    //         },
+    //         breaks: true
+    //     })
+    // }, [])
+//     useEffect(() => {
+//     marked.setOptions({
+//         highlight: function (code, lang) {
+//             if (lang && hljs.getLanguage(lang)) {
+//                 return hljs.highlight(code, { language: lang }).value;
+//             }
+//             // Force javascript as default language if none specified
+//             return hljs.highlight(code, { language: 'javascript' }).value;
+//         },
+//         breaks: true,
+//         langPrefix: 'hljs language-'
+//     });
+// }, []);
+
+//     // Function to convert response to HTML with syntax highlighting
+//     const formatResponse = (text) => {
+//         return marked(text)
+//     }
+useEffect(() => {
+     marked.use(markedHighlight({
+            langPrefix: 'hljs language-',
+            highlight: (code, language) => {
+                if (language && hljs.getLanguage(language)) {
+                    try {
+                        return hljs.highlight(code, { language }).value;
+                    } catch (err) {
+                        console.error(err);
+                    }
+                }
+                return hljs.highlightAuto(code).value;
+            }
+        }));
+
+        // Additional marked options
+        marked.setOptions({
+            breaks: true,
+            gfm: true
+        });
+    }, []);
+
+    const formatResponse = (text) => {
+        try {
+            return marked(text);
+        } catch (err) {
+            console.error('Markdown parsing error:', err);
+            return text;
+        }
+    }
     return (
 
         <div className="main">
@@ -51,7 +114,7 @@ const Main = () => {
                                     <hr />
                                     <hr />
                                 </div>
-                                : <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                                : <p dangerouslySetInnerHTML={{ __html: formatResponse(resultData) }}></p>
                             }
                         </div>
                     </div>}
